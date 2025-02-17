0e05cab5d0ae40ed6133ca7667221478368026cd
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-02-17 22:19:12 +0530

    Remove Consecutive Subsequences

---
+++ Stacks/removeConsecutiveSubsequences.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class removeConsecutiveSubsequences{
+    public static int[] remove(int[] arr){
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(st.size()==0 || st.peek()!=arr[i])
+                st.push(arr[i]);
+            else if(arr[i]==st.peek()){
+                if(i==n-1 || arr[i]!=arr[i+1]) st.pop();
+            }
+        }
+        int[] res=new int[st.size()];
+        for(int i=res.length-1;i>=0;i--){
+            res[i]=st.pop();
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,2,3,3,10,10,10,4,4,4,3,6};
+        int[] res=remove(arr);
+        for(int i=0;i<res.length;i++){
+            System.out.print(res[i]+" ");
+        }
+    }
+}
