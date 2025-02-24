d81369438318e427b7ff027cdcc2db0882115b8a
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-02-24 22:36:29 +0530

    Infix to prefix

--- Stacks/prefix.java
+++ Stacks/prefix.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class prefix {
     public static void main(String[] args) {
-        String str="9-5+3*4/6";
+        String str="9-(5+3)*4/6";
         Stack<String> val=new Stack<>();
         Stack<Character> op=new Stack<>();
         for(int i=0;i<str.length();i++)
