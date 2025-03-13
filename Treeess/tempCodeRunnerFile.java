2d6e51347c12ec009492754e3b8f91c0d290b85d
coderharpuneet <harpuneet2007.be23@chitkara.edu.in>
2025-03-13 23:57:08 +0530

    Implementation of trees

---
+++ Treeess/tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+Node a=new Node(1);
+        Node b=new Node(2);
+        Node c=new Node(3);
+        Node d=new Node(4);
+        Node e=new Node(5);
+        Node f=new Node(6);
+        a.left=b;
+        a.right=c;
+        b.left=d;
+        b.right=e;
+        e.right=f;
